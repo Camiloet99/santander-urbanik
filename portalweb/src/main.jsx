70a3716b9278a8e4cf18c798167ef586e8dbca17
LuzDD <luz.durango@upb.edu.co>
2025-12-02 22:01:54 -0500

    mapa y filtros bsd

--- portalweb/src/main.jsx
+++ portalweb/src/main.jsx
@@ -1,16 +1,14 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-// 🔽 comenta App y usa Mapa
-// import App from "./App";
+import App from "./App";
 import "./index.css";
 import { AuthProvider } from "@/context/AuthContext";
-import Mapa from "./pages/Mapa"; // <-- si Mapa.jsx está en src/pages
-// o: import Mapa from "./components/Mapa";  si está en src/components
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <Mapa />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
